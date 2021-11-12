2d9915fc8d75a28869cf9dba208d25fa7c5d116d
michael barrett <michaelbarrett343@googlemail.com>
2021-11-12 17:13:07 +0000

    basic functionality with run btn

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const input = document.getElementById("num-input");
+const btnrun = document.querySelector(".btn-run");
+const iterations = document.querySelector(".iterations")
+
 const collatz = (number) => {
-    let acc = 0;
-    console.log("Starting Number is", number)
-    while (number > 1) {
+  let acc = 0;
+  iterations.insertAdjacentHTML("beforeEnd",`<li>"Starting Number is"${number}</li>`);
+  while (number > 1) {
     number = number % 2 === 0 ? number / 2 : 3 * number + 1;
-    console.log(number);
+    iterations.insertAdjacentHTML("beforeEnd",`<li>${number}</li>`);
     acc += 1;
+  }
+  iterations.insertAdjacentHTML("beforeEnd",`<li>${acc} Iterations </li>`);
 }
-console.log(`${acc} iterations`)
-}
+btnrun.addEventListener("click", (e) => {
+    collatz(input.value)
+})
