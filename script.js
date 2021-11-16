48c6a92fcbd3f6b811365a53635d08d4ecafcb38
michael barrett <michaelbarrett343@googlemail.com>
2021-11-16 13:48:42 +0000

    added submit and reset button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const input = document.getElementById("num-input");
 const btnrun = document.querySelector(".btn-run");
-const iterations = document.querySelector(".iterations")
+const iterations = document.querySelector(".iterations");
+const form = document.querySelector(".form");
+const resetBtn = document.querySelector(".reset");
 
+const reset = () => {
+  iterations.innerHTML = " ";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
 const collatz = (number) => {
+  reset();
   let acc = 0;
   iterations.insertAdjacentHTML("beforeEnd",`<li>"Starting Number is"${number}</li>`);
   while (number > 1) {
@@ -14,4 +24,9 @@ const collatz = (number) => {
 }
 btnrun.addEventListener("click", (e) => {
     collatz(input.value)
+})
+
+resetBtn.addEventListener("click", (e) => {
+  reset()
+  input.value = 0;
 })
